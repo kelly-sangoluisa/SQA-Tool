86748f375acd0bb783e8381173411eb8aa44f360
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2026-01-10 21:41:03 -0500

    chore: update package.json to add testing scripts and dependencies

---
+++ frontend/jest.config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.ts?(x)',
+    '**/?(*.)+(spec|test).ts?(x)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.tsx',
+  ],
+}
+
+module.exports = createJestConfig(customJestConfig)
